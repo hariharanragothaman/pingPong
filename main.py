016459aac2fc7158442ccdbea128483d46e3e9d6
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-10-14 16:24:04 -0400

    Adding some more game logic to move the paddles up and down

--- main.py
+++ main.py
@@ -1,35 +1,72 @@
+# Import the pygame library and initialise the game engine
 import pygame
+from paddle import Paddle
 
-# Initializing the game engine
 pygame.init()
 
-# Defining some colors
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
+# Define some colors
+BLACK = (0,0,0)
+WHITE = (255,255,255)
 
-# Opening a new window
+# Open a new window
 size = (700, 500)
 screen = pygame.display.set_mode(size)
-pygame.display.set_caption("PONG")
+pygame.display.set_caption("Pong")
 
-# Flag to run Game loop until user exits game window!
-carry_on = True
+paddleA = Paddle(WHITE, 10, 100)
+paddleA.rect.x = 20
+paddleA.rect.y = 200
+
+paddleB = Paddle(WHITE, 10, 100)
+paddleB.rect.x = 670
+paddleB.rect.y = 200
+
+#This will be a list that will contain all the sprites we intend to use in our game.
+all_sprites_list = pygame.sprite.Group()
+
+# Add thepaddles to the list of sprites
+all_sprites_list.add(paddleA)
+all_sprites_list.add(paddleB)
+
+# The loop will carry on until the user exit the game (e.g. clicks the close button).
+carryOn = True
 
 # The clock will be used to control how fast the screen updates
 clock = pygame.time.Clock()
 
-while carry_on:
-    # -- Main event loop
-    for event in pygame.event.get(): # user did something
+# -------- Main Program Loop -----------
+while carryOn:
+    # --- Main event loop
+    for event in pygame.event.get(): # User did something
         if event.type == pygame.QUIT: # If user clicked close
-            carry_on = False
+            carryOn = False # Flag that we are done so we exit this loop
+        elif event.type==pygame.KEYDOWN:
+            if event.key==pygame.K_x: #Pressing the x Key will quit the game
+                carryOn=False
 
-    # -------- Game logic should go here
+    #Moving the paddles when the user uses the arrow keys (player A) or "W/S" keys (player B)
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        paddleA.moveUp(5)
+    if keys[pygame.K_s]:
+        paddleA.moveDown(5)
+    if keys[pygame.K_UP]:
+        paddleB.moveUp(5)
+    if keys[pygame.K_DOWN]:
+        paddleB.moveDown(5)
+
+        # --- Game logic should go here
+    all_sprites_list.update()
+
+
     # --- Drawing code should go here
     # First, clear the screen to black.
     screen.fill(BLACK)
     #Draw the net
     pygame.draw.line(screen, WHITE, [349, 0], [349, 500], 5)
+
+    #Now let's draw all the sprites in one go. (For now we only have 2 sprites!)
+    all_sprites_list.draw(screen)
 
     # --- Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
